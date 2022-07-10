daebe92d1dc33272410a851611aa31aa2dc4390f
carlo-ddm <carloduminuco90@gmail.com>
2022-07-10 14:31:35 +0200

    imposto e lancio il seeder

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\Category;
+use Illuminate\Support\Str;
 class CategoriesTableSeeder extends Seeder
 {
     /**
@@ -11,6 +12,12 @@ class CategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $data = ['HTML', 'CSS', 'Javascript', 'PHP', 'C++'];
+        foreach ($data as $categories) {
+            $new_category = new Category();
+            $new_category->name = $categories;
+            $new_category->slug = Str::slug($categories. '-');
+            $new_category->save();
+        }
     }
 }
